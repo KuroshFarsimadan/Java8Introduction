d05233b72792fec047c3d9475527228b2c5cab6d
Kurosh Farsimadan <kurosh_farsimadan@yahoo.com>
2019-04-12 08:38:53 -0400

    Add files via upload

--- Java8Introduction/src/AnonymousClassGermanShepherd.java
+++ Java8Introduction/src/AnonymousClassGermanShepherd.java
@@ -1,14 +1,20 @@
-
 class Dog {
 	public void bark() {
 		System.out.println("Woof");
 	}
 }
 
+// Functional interfaces provide target types for lambda
+// expressions and method references. This is a Java 8
+// feature
+interface DogInterface {
+	String bark(String barking);
+}
+
 public class AnonymousClassGermanShepherd {
 
 	public static void main(String[] args) {
-		
+
 		// Child anonymous inner class overriding parent
 		Dog anonymousInnerClass = new Dog() {
 			public void bark() {
@@ -16,6 +22,14 @@ public class AnonymousClassGermanShepherd {
 			}
 		};
 		anonymousInnerClass.bark();
+
+		// String tester = "Woofie";
+
+		DogInterface lambdaJava8Way = (tester) -> {
+			return tester;
+		};
+
+		System.out.println(lambdaJava8Way.bark("Woofie"));
 
 	}
 
